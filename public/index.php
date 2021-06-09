b8c414a7120962cae0d9a826c2b201ecd75e00ce
Garnik Gasparyan <gasp.garnik@gmail.com>
2021-06-09 08:10:24 +0400

    Initialize project

---
+++ public/index.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once __DIR__ . "/../vendor/autoload.php";
+
+use Symfony\Component\HttpFoundation\Request;
+use App\Middlewares\LayeredMiddlewaresHub;
+use App\Middlewares\RoutingMiddleware;
+
+$layeredMiddlewaresHub = new LayeredMiddlewaresHub();
+// Add layers (Middlewares of MiddlewareInterface type) to LayeredMiddlewares!
+$layeredMiddlewaresHub
+    ->add(new RoutingMiddleware());
+
+// request processing by layered middlewares
+$request  = Request::createFromGlobals();
+$response = $layeredMiddlewaresHub->dive($request);
+$response->send();
