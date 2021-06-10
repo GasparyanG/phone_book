7ae2866e6c540493782b8759b20da4ec34ffa15c
Garnik Gasparyan <gasp.garnik@gmail.com>
2021-06-10 12:20:13 +0400

    Handle record deletion

--- src/Models/DeletingStrategy.php
+++ src/Models/DeletingStrategy.php
@@ -5,6 +5,7 @@ namespace App\Models;
 
 
 use App\Database\Entities\Contact;
+use App\Services\API\JsonAPI\Resource;
 use Symfony\Component\HttpFoundation\Request;
 
 class DeletingStrategy extends ContactManipulator
@@ -14,13 +15,30 @@ class DeletingStrategy extends ContactManipulator
         if (!$this->resourceExists($placeholders[Contact::ID]))
             return $this->notFound();
 
-        $content = $req->getContent();
-        $decodedContent = json_decode($content, true);
+        $deletedRecord = $this->prepareResource($placeholders[Contact::ID]);
+        $this->delete($placeholders[Contact::ID]);
 
-        $violations = $this->validate($decodedContent);
-        if (count($violations) > 0)
-            return $this->error($violations);
+        return $deletedRecord;
+    }
 
-        return $this->success($this->update($decodedContent, $placeholders));
+    protected function prepareResource(int $id): array
+    {
+        $contact = $this->em->getRepository(Contact::class)->find($id);
+
+        $resource = new Resource();
+        $resource->setId($contact->getId());
+        $resource->setType(Contact::$table_name);
+
+        $resource->arrayRepresentation();
+        $representation = $resource->getRepresentation();
+        unset($representation[Resource::DATA][Resource::ATTRIBUTES]);
+        return $representation;
+    }
+
+    protected function delete(int $id): void
+    {
+        $contact = $this->em->getRepository(Contact::class)->find($id);
+        $this->em->remove($contact);
+        $this->em->flush();
     }
 }
